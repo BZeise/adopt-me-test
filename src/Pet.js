af839dd098bb568e0cdbf2d6e0f2221480e0c3d8
Ben Zeise <ben.zeise@gmail.com>
2022-03-01 13:48:14 -0600

    caught up to section 03 but has problems

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-// const Pet = (props) => {
-export default function Pet({ name, animal, breed }) {
+const Pet = (props) => {
+  //export default function Pet({ name, animal, breed }) {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
-}
+};
+
+export default Pet;
