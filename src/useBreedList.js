1aece117423354d010a8da908334f18dc7ed9fe7
Ben Zeise <ben.zeise@gmail.com>
2022-03-01 15:45:44 -0600

    whenever animal changes, calls and updates

---
+++ src/useBreedList.js
@@ -0,0 +1,32 @@
+import { useState, useEffect, useRef } from "react";
+
+const localCache = {};
+// we're just using an object, but of course this should be localStorage or something similar
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-vs.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+}
