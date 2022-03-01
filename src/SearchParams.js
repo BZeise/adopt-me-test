808c8f57e9bd15ef2a4d8a6f5c1a5bfc5c5e1d00
Ben Zeise <ben.zeise@gmail.com>
2022-03-01 14:49:48 -0600

    added lots of hook code, added map function

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   //   const location = "Seattle, WA";
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -16,6 +19,32 @@ const SearchParams = () => {
           />
           {/* React hooks simulate browser events.
           They can perform the work much faster than browsers can. */}
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlue={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+              return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+
+            {/* NOTE:  Any time you have a list of data that you're turning into a list of components, use .map(). */}
+          </select>
         </label>
         <button>Submit</button>
       </form>
