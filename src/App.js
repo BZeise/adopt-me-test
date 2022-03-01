f02b74d8ac3298cb34d2c6a1760de35ad7df69bd
Ben Zeise <ben.zeise@gmail.com>
2022-03-01 11:30:44 -0600

    added some prettier stuff but it's not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 const Pet = (props) => {
-    return (
-        React.createElement("div", {}, [
-            React.createElement("h1", {}, props.name),
-            React.createElement("h2", {}, props.animal),
-            React.createElement("h2", {}, props.breed),
-        ])
-    )
+return (
+React.createElement("div", {}, [
+React.createElement("h1", {}, props.name),
+React.createElement("h2", {}, props.animal),
+React.createElement("h2", {}, props.breed),
+])
+)
 }
 
 // code goes here
@@ -13,34 +13,34 @@ const Pet = (props) => {
 // ohhhhh so hot
 //  Command+/ to comment
 const App = () => {
-    //  React wants to re-render these very frequently
-    // So, these functions have to be fast.
-    // They should be "pure", in functional programming lingo.
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Lenny!!",
-                animal: "Dog",
-                breed: "Goldendoodle",
-            }),
-            React.createElement(Pet, {
-                name: "Saorise!!",
-                animal: "Dog",
-                breed: "Mutt",
-            }),
-            React.createElement(Pet, {
-                name: "Bruce!!",
-                animal: "Cat",
-                breed: "Batman",
-            }),
-        ]
-    );
+//  React wants to re-render these very frequently
+// So, these functions have to be fast.
+// They should be "pure", in functional programming lingo.
+return React.createElement(
+"div",
+{},
+[
+React.createElement("h1", {}, "Adopt Me!"),
+React.createElement(Pet, {
+name: "Lenny!!",
+animal: "Dog",
+breed: "Goldendoodle",
+}),
+React.createElement(Pet, {
+name: "Saorise!!",
+animal: "Dog",
+breed: "Mutt",
+}),
+React.createElement(Pet, {
+name: "Bruce!!",animal: "Cat",breed: "Batman",
+}),
+]
+);
 };
 
 // we'll call ReactDOM.render exactly once
 // but we'll call React.createElement thousands of times
 // heads up:  This will be renamed 'ReactDOM.createRoot' in React v18
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+// adding comments to fire Prettier
