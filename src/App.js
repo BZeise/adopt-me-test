2d1b9163a8211de5d8b7122c64e85397e403287b
Ben Zeise <ben.zeise@gmail.com>
2022-03-02 11:50:32 -0600

    expanded Details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 // import Pet from "./Pet"; // now obsolete, only called by Results
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -11,7 +11,9 @@ const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <h1>Adopt Mehhhhhy!</h1>
+        <header>
+          <Link to="/">Adopt Mehhhhhy!</Link>
+        </header>
         <Routes>
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,68 @@
-const Details = () => {
-  return <h2>ohhhhh sweet! Check out these DEETS</h2>;
-};
+import { Component, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+// const Details = () => {
+//   const { id } = useParams();
+//   // Using BrowserRouter
+//   // because I'm a route, I know I can pull parameters from the URL
+//   // so I know what my id is, because we've assigned it
+//   return <h2>ohhhhh sweet! Check out these DEETS!! ID = {id}</h2>;
+// };
+
+class Details extends Component {
+  // you can't use hooks in class components
+  // so the first thing we do is write a constructor
+  // constructor takes in `props`
+  constructor(props) {
+    super(props); // SUPERPROPSSSSS
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    // after the initial Render is complete, we'll call componentDidMount
+
+    // fetch is a function built-in to browsers
+    // more than powerful enough for simple GETs
+    // it's a little funny to call `await` twice, but we get used to it
+    const res = await fetch()(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+
+    const json = await res.json();
+
+    // we used to call useState.  but now we just have one function, "setState"
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+
+    // this.setState({
+    //   loading: false,
+    // });
+
+    // this.setState(json.pets[0]);
+  }
+
+  // the only rule of class components is that they must have a Render function
+  render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    // desconstruct here
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} -- {breed} -- {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
