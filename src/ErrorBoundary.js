3bafc06707fe4c714736c83a76347d059d2999ca
Ben Zeise <ben.zeise@gmail.com>
2022-03-02 14:07:02 -0600

    error boundary working, throwing new Error

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,17 +1,28 @@
 // mostly code from reactjs.org/docs/error-boundaries.html
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
   }
-  render() {
+
+  // you can use this for any component
+  // you can think of it as a useEffect that responds to any state change on this component
+  // componentDidUpdate does not run at start
+  componentDidUpdate() {
     if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 2000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Navigate to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           There was an error with this listing. <Link to="/">Click here</Link>{" "}
